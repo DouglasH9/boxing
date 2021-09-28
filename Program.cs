08cc2c9d701726da486156c44cc029bf036f42aa
Doug Herman <douglash9882@gmail.com>
2021-09-27 23:28:33 -0500

    boxing/unboxing assignment

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> objectList = new List<object>();
+            objectList.Add(7); 
+            objectList.Add(28); 
+            objectList.Add(-1); 
+            objectList.Add(true);
+            objectList.Add("chair");
+
+            int sum = 0;
+            foreach (object item in objectList)
+            {
+                Console.WriteLine(item);
+                if (item is int)
+                {
+                    sum += (int)item;
+                }
+                
+                
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
